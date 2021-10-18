b566f7889994ddb892636ac0582d9e44907076cd
martinlaursen97 <martin_laursen21@hotmail.com>
2021-10-18 20:38:32 +0200

    fist commit

---
+++ src/main/java/com/opgave/aflevering/AfleveringApplication.java
@@ -0,0 +1,13 @@
+package com.opgave.aflevering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfleveringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfleveringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/opgave/aflevering/AfleveringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opgave.aflevering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfleveringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
