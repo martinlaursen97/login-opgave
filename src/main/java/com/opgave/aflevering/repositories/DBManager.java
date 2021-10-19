ded629ceb55d16574f89fe2745be3a3f183576ae
martinlaursen97 <82436992+martinlaursen97@users.noreply.github.com>
2021-10-19 15:40:11 +0200

    Update DBManager.java

--- src/main/java/com/opgave/aflevering/repositories/DBManager.java
+++ src/main/java/com/opgave/aflevering/repositories/DBManager.java
@@ -16,7 +16,7 @@ public class DBManager {
 
     public static Connection getConnection(){
         if (connection != null) return connection;
-        try (InputStream input = new FileInputStream("C:\\Users\\Snab\\IdeaProjects\\login-opgave\\src\\main\\resources\\application.properties")) {
+        try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
             Properties properties = new Properties();
             properties.load(input);
             url = properties.getProperty("url");
